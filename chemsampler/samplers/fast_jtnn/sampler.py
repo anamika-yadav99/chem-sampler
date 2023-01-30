7543d8d901a057f68969ab62af2536b5986d7e2f
anamika-yadav99 <anamika310.yada@gmail.com>
2023-01-30 21:14:15 +0530

    custom sampler to search from pre-calculated molecules added

--- chemsampler/samplers/fast_jtnn/sampler.py
+++ chemsampler/samplers/fast_jtnn/sampler.py
@@ -1,1 +1,32 @@
 from ...tools.fast_jtnn.sampler import JtnnSampler
+
+from ...tools.fpsim2.searcher import SimilaritySearcher, RandomSearcher
+import os
+
+class JtnnSampler:
+    def __init__(self):
+        #TODO: pre-calculate samples
+        self.fp_filename = '../chemsampler/data/pre_calculated/fast_jtnn/.h5'
+        self.db_smiles_filename = '../chemsampler/data/pre_calculated/fast_jtnn/.csv'
+
+        if os.path.exists(self.fp_filename) is False:
+            smiles_list = SimilaritySearcher(self.fp_filename).read_db_smiles()
+            SimilaritySearcher(self.fp_filename).fit(smiles_list)
+
+    def Sampler(self, smiles_list, n,  search_pre_calculated=True, cutoff = 0.7):
+        if search_pre_calculated==True:
+            samples = []
+            for smile in smiles_list:
+                _samples = SimilaritySearcher(self.fp_filename).search(smile, cutoff=0.7)
+                if len(_samples) == 0:
+                    print('Coudn\'t find samples in the cutoff range : {}. Generating random samples:'.format(cutoff))
+                    _samples = RandomSearcher(self.db_smiles_filename).search(n)
+                samples += _samples 
+        
+        else:
+            sampler = JtnnSampler()
+            samples = sampler.sample(n)
+
+        return samples
+
+
--- chemsampler/samplers/moler/sampler.py
+++ chemsampler/samplers/moler/sampler.py
@@ -1,1 +1,31 @@
 from ...tools.moler.sampler import MolerSampler
+from ...tools.fpsim2.searcher import SimilaritySearcher, RandomSearcher
+import os
+
+class MolerSampler:
+    def __init__(self):
+        #TODO: Come up with a better way to store precalculated files
+        self.fp_filename = '../chemsampler/data/pre_calculated/moler/moler_100k.h5'
+        self.db_smiles_filename = '../chemsampler/data/pre_calculated/moler/moler_10k.csv'
+
+        if os.path.exists(self.fp_filename) is False:
+            smiles_list = SimilaritySearcher(self.fp_filename).read_db_smiles()
+            SimilaritySearcher(self.fp_filename).fit(smiles_list)
+
+    def Sampler(self, smiles_list, n,  search_pre_calculated=True, cutoff = 0.7):
+        if search_pre_calculated==True:
+            samples = []
+            for smile in smiles_list:
+                _samples = SimilaritySearcher(self.fp_filename).search(smile, cutoff=0.7)
+                if len(_samples) == 0:
+                    print('Coudn\'t find samples in the cutoff range : {}. Generating random samples:'.format(cutoff))
+                    _samples = RandomSearcher(self.db_smiles_filename).search(n)
+                samples += _samples 
+        
+        else:
+            sampler = MolerSampler()
+            samples = sampler.sample(n)
+
+        return samples
+
+
